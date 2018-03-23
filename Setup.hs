e736406236239c30f0faca2f7ce05020553e834b
Vikrem <me@vikrem.com>
2018-03-22 23:26:00 -0400

    cabal won't take relative library paths :(

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,45 @@
+-- All this junk is to just add two paths to library-paths
+
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup (ConfigFlags)
+import Distribution.Simple.LocalBuildInfo
+    ( LocalBuildInfo
+    , configFlags
+    , localPkgDescr
+    )
+import Data.Maybe
+    ( fromJust
+    , fromMaybe
+    )
+import System.Directory
+    ( doesDirectoryExist
+    , getCurrentDirectory
+    , removeDirectoryRecursive
+    )
+import qualified Distribution.PackageDescription as PD
+
+main = defaultMainWithHooks simpleUserHooks {
+  confHook = libConfHook
+  }
+
+libConfHook :: (PD.GenericPackageDescription, PD.HookedBuildInfo) -> ConfigFlags -> IO LocalBuildInfo
+libConfHook (description, buildInfo) flags = do
+    localBuildInfo <- confHook simpleUserHooks (description, buildInfo) flags
+    let packageDescription = localPkgDescr localBuildInfo
+        library = fromJust $ PD.library packageDescription
+        libraryBuildInfo = PD.libBuildInfo library
+    dir <- getCurrentDirectory
+    return localBuildInfo {
+        localPkgDescr = packageDescription {
+            PD.library = Just $ library {
+                PD.libBuildInfo = libraryBuildInfo {
+                    PD.extraLibDirs = (map (dir ++) extra_dirs) ++ PD.extraLibDirs libraryBuildInfo
+                }
+            }
+        }
+    }
+    where
+      extra_dirs = [
+        "/vendor/ChakraCoreFiles_1_8_2_OSX/lib",
+        "/vendor/ChakraCoreFiles_1_8_2_Linux/lib"
+        ]
