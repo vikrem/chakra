26e2722611260795469ed8af0049aa32249c8191
Vikrem <me@vikrem.com>
2018-03-29 18:39:26 -0400

    Get your libs yourself thx

--- src/Chakra.hs
+++ src/Chakra.hs
@@ -1,6 +1,25 @@
-module Chakra
-    ( someFunc
-    ) where
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Chakra where
+
+import qualified Language.C.Inline as C
+
+C.include "ChakraCommon.h"
+C.include "ChakraCoreVersion.h"
+C.include "ChakraDebug.h"
+C.include "ChakraCore.h"
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  [C.block| void {
+      JsRuntimeHandle runtime;
+      JsContextRef context;
+      JsValueRef result;
+      JsCreateRuntime(JsRuntimeAttributeNone, NULL, &runtime);
+      JsCreateContext(runtime, &context);
+      JsSetCurrentContext(context);
+      JsSetCurrentContext(JS_INVALID_REFERENCE);
+      JsDisposeRuntime(runtime);
+  } |]
+
+  putStrLn "ye"
