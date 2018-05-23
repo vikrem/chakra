f2a7a4717145692127ee5a0de3647d6c2a817598
Vikrem <me@vikrem.com>
2018-05-22 23:21:22 -0700

    Finalize

--- src/Chakra/Typescript.hs
+++ src/Chakra/Typescript.hs
@@ -20,10 +20,10 @@ import GHC.Generics
 import Data.Time.Clock (UTCTime)
 import Data.UUID.Types (UUID)
 
-
 symtext :: forall a. KnownSymbol a => Proxy a -> T.Text
 symtext _ = T.pack $ symbolVal (Proxy @a)
 
+-- map hs types to ts type strings
 type family TsType a :: Symbol where
   TsType Int = "number"
   TsType Integer = "number"
@@ -39,6 +39,11 @@ type family TsType a :: Symbol where
   TsType (JsPromise a) = AppendSymbol "Promise<" (AppendSymbol (TsType a) ">")
   TsType [a] = (AppendSymbol (TsType a) "[]")
   TsType a = (GTsType (Rep a))
+
+-- The final return type of a native function that returns an `a` to js
+type family RetTsType a :: * where
+  RetTsType (JsPromise a) = JsPromise a
+  RetTsType typ = IO typ
 
 class GHasTsType (r :: * -> *) where
   type GTsType r :: Symbol
@@ -58,24 +63,6 @@ instance (GHasTsType a, GHasTsType b) => GHasTsType (a :*: b) where
 instance (GHasTsType a, GHasTsType b) => GHasTsType (a :+: b) where
   type GTsType (a :+: b) = AppendSymbol (GTsType a) (AppendSymbol " | " (GTsType b))
 
-data TsFunctionDecl = TsFunctionDecl {
-  fnName :: T.Text,
-  fnTypes :: [T.Text],
-  fnVarNames :: [T.Text],
-  fnRetType :: T.Text
-  } deriving (Show, Eq, Ord, Generic)
-
-data Injectible = Namespace T.Text [Injectible] |
-  forall a. (HasTSDecl (JsType a), JsTypeable a) => HsFunc T.Text a
-
-instance Monoid TsFunctionDecl where
-  mempty = TsFunctionDecl "" [] [] ""
-  mappend (TsFunctionDecl a b c d) (TsFunctionDecl a' b' c' d') =
-    TsFunctionDecl (a <> a') (b <> b') (c <> c') (d <> d')
-
-class HasTSDecl a where
-  tsDecl :: Proxy a -> TsFunctionDecl
-
 -- API building
 
 data NS (n :: Symbol) (a :: [*])
@@ -93,19 +80,19 @@ indent s = T.unlines $ ("  " <> ) <$> T.lines s
 class HasTSSpec (a :: b) where
   tsSpec :: Proxy a -> T.Text
 
-class HasTSSpecList (a :: [*]) where
+class AllHasTSSpec (a :: [*]) where
   tsSpecList :: Proxy a -> [T.Text]
 
-instance HasTSSpecList '[] where
+instance AllHasTSSpec '[] where
   tsSpecList _ = []
 
 instance (HasTSSpec x, HasTSSpec y) => HasTSSpec (x :+ y) where
   tsSpec _ = tsSpec (Proxy @x) <> tsSpec (Proxy @y)
 
-instance (HasTSSpec x, HasTSSpecList xs) => HasTSSpecList (x ': xs) where
+instance (HasTSSpec x, AllHasTSSpec xs) => AllHasTSSpec (x ': xs) where
   tsSpecList _ = (tsSpec (Proxy @x)) : (tsSpecList (Proxy @xs))
 
-instance (HasTSSpecList xs, KnownSymbol n) => HasTSSpec (NS n xs) where
+instance (AllHasTSSpec xs, KnownSymbol n) => HasTSSpec (NS n xs) where
   tsSpec _ = "declare namespace " <> (T.pack $ symbolVal $ Proxy @n) <> " {\n" <>
     T.concat (indent <$> (tsSpecList $ Proxy @xs)) <>
     "}"
@@ -123,21 +110,11 @@ instance {-# OVERLAPS #-} (KnownSymbol (TsType t),
 
 instance (HasTSSpec xs, KnownSymbol n, KnownSymbol (TsType t)) => HasTSSpec (Arg n t :> xs) where
   tsSpec _ = (symtext $ Proxy @n) <> ": " <>
-    symtext (Proxy @(TsType t)) <> ")" <>
+    symtext (Proxy @(TsType t)) <>
     tsSpec (Proxy @xs)
 
 instance KnownSymbol (TsType t) => HasTSSpec (Ret t) where
-  tsSpec _ = ": " <> symtext (Proxy @(TsType t))
-
-instance (KnownSymbol (TsType a)) => HasTSDecl (JsFnTypelist '[] a) where
-  tsDecl _ = mempty {fnRetType = typeName}
-    where
-      typeName = T.pack $ symbolVal $ Proxy @(TsType a)
-
-instance {-# OVERLAPS #-} (KnownSymbol (TsType a)) => HasTSDecl (JsFnTypelist '[] (JsPromise a)) where
-  tsDecl _ = mempty {fnRetType = "Promise<" <> typeName <> ">" }
-    where
-      typeName = T.pack $ symbolVal $ Proxy @(TsType a)
+  tsSpec _ = "): " <> symtext (Proxy @(TsType t))
 
 class HasTSImpl a where
   type TSImpl a :: *
@@ -147,14 +124,17 @@ class HasTSImpl a where
 class AllHasTSImpl (a :: [*]) where
   injectAllAPI :: Proxy a -> [T.Text] -> TSImplList a -> Chakra ()
 
-instance (TypeError ('Text "Can't solve for the implementation of an empty namespace")) => AllHasTSImpl '[]
+instance (TypeError ('Text "Can't solve for the implementation of an empty namespace")) => AllHasTSImpl '[] where
+  injectAllAPI = undefined
+
 instance {-# OVERLAPS #-} (HasTSImpl a) => AllHasTSImpl '[a] where
-  injectAllAPI _ xs x = injectAPI (Proxy @a) xs x
+  injectAllAPI _ = injectAPI (Proxy @a)
+
 instance (HasTSImpl a,
           AllHasTSImpl xs,
           TSImplList (a ': xs) ~ (TSImpl a :+ TSImplList xs)
           ) => AllHasTSImpl (a ': xs) where
-  injectAllAPI _ xs (x :+ rest) = injectAPI (Proxy @a) xs x
+  injectAllAPI _ xs (x :+ rest) = injectAPI (Proxy @a) xs x >> injectAllAPI (Proxy @xs) xs rest
 
 type family TSImplList (a :: [*]) where
   TSImplList '[] = TypeError ('Text "Can't solve for the implementation of an empty namespace")
@@ -165,8 +145,8 @@ instance (HasTSImpl x, HasTSImpl y) => HasTSImpl (x :+ y) where
   type TSImpl (x :+ y) = (TSImpl x) :+ (TSImpl y)
   injectAPI _ ls (x :+ y) = injectAPI (Proxy @x) ls x >> injectAPI (Proxy @y) ls y
 
-instance JsTypeable a => HasTSImpl (Ret a) where
-  type TSImpl (Ret a) = a
+instance JsTypeable (RetTsType a) => HasTSImpl (Ret a) where
+  type TSImpl (Ret a) = RetTsType a
 -- We need a function name!
   injectAPI _ [] _ = undefined --injectChakra x ls ""
   injectAPI _ xs x = injectChakra x (init xs) (last xs)
@@ -181,49 +161,12 @@ instance (JsTypeable (TSImpl xs), HasTSImpl xs, KnownSymbol n) => HasTSImpl (Fn 
   type TSImpl (Fn n :> xs) = TSImpl xs
   injectAPI _ xs = injectAPI (Proxy @xs) (xs ++ [symtext $ Proxy @n])
 
-
 instance (KnownSymbol n, AllHasTSImpl xs) => HasTSImpl (NS n xs) where
   type TSImpl (NS n xs) = TSImplList xs
   injectAPI _ xs = injectAllAPI (Proxy @xs) (xs ++ [symtext $ Proxy @n])
 
+genTS :: HasTSSpec spec => Proxy spec -> T.Text
+genTS = tsSpec
 
-instance (KnownSymbol (TsType a),
-          HasTSDecl (JsFnTypelist xs b)
-         ) => HasTSDecl (JsFnTypelist (a ': xs) b) where
-  tsDecl _ = mempty {fnTypes = [typeName]} <> tsDecl (Proxy @(JsFnTypelist xs b))
-    where
-      typeName = T.pack $ symbolVal $ Proxy @(TsType a)
-
-indentBlock :: T.Text -> T.Text
-indentBlock = T.unlines . map ("  " <>)  . T.lines
-
-printInjectible :: Injectible -> T.Text
--- TLD declare
-printInjectible (Namespace nsName inj) = "declare " <> printInjectible' (Namespace nsName inj)
-printInjectible x = printInjectible' x
-
-printInjectible' :: Injectible -> T.Text
-printInjectible' (HsFunc name f) = printDecl $ mempty { fnName = name } <> getDecl f
-printInjectible' (Namespace nsName inj) = T.unlines [
-    "namespace " <> nsName <> " {"
-  , T.unlines $ indentBlock . printInjectible' <$> inj
-  , "}"
-  ]
-
-injectInjectible :: Injectible -> Chakra ()
-injectInjectible = injectInjectible' []
-
-injectInjectible' :: [T.Text] -> Injectible -> Chakra ()
-injectInjectible' ns (HsFunc name f) = injectChakra f ns name
-injectInjectible' ns (Namespace name injs) = sequence_ $ injectInjectible' (ns ++ [name]) <$> injs
-
-getDecl :: forall a b. (JsType a ~ b, HasTSDecl b) => a -> TsFunctionDecl
-getDecl _ = tsDecl (Proxy @b)
-
-printDecl :: TsFunctionDecl -> T.Text
-printDecl TsFunctionDecl{..} =
-  "function " <> fnName <> "(" <> argList <> "): " <> fnRetType <> ";"
-  where
-    argList :: T.Text
-    argList = T.intercalate ", " $
-      zipWith (\n t -> n <> ": " <> t) (fnVarNames <> (T.singleton <$> ['a'..'z'])) fnTypes
+injectNativeAPI :: (HasTSSpec bindings, HasTSImpl bindings) => Proxy bindings -> TSImpl bindings -> Chakra ()
+injectNativeAPI prox = injectAPI prox []
